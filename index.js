decb3b904f450d512ab24b1bbb390b1383183b60
web-dev <nathankim137311@gmail.com>
2021-12-31 17:55:57 -0800

    added schema for tables and modified scope of api project

--- index.js
+++ index.js
@@ -4,35 +4,90 @@ const cheerio = require('cheerio');
 const puppeteer = require('puppeteer');
 const cors = require('cors');
 const { db } = require('./database');
-
 const { v4: uuidv4 } = require('uuid');
 
 const PORT = 8000; 
 
-// docker run --name some-mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=my-secret-pw -d mysql
-
-const youtubers = [
-    {
-        channelId: 'UC-lHJZR3Gqxm24_Vd_AJ5Yw',
-        channelName: 'PewDiePie',
-        merchStore: 'https://represent.com/store/pewdiepie', 
-    },
-    {
-        channelId: 'UCO1ITICo8MLHGAXR1uzFwjA',
-        channelName: 'OneyPlays',
-        merchStore: 'https://sharkrobot.com/collections/oney', 
-    },
+const comedyPodcasts = [
     {
         channelId: 'UCIyIoM_Nd8HtY19fuR_ov2A',
         channelName: 'TigerBelly',
-        merchStore: 'https://thetigerbelly.com/collections/all',
+        storeUrl: 'https://thetigerbelly.com/collections/all',
     },
     {
         channelId: 'UCGCXWfqaovf_vI05iW6pLdA',
         channelName: 'ScissorBros',
-        merchStore: 'https://shop.upstatemerch.com/scissorbros/shop/home',
-    }
-]
+        storeUrl: 'https://shop.upstatemerch.com/scissorbros/shop/home',
+    },
+    {
+        channelId: 'UCYIgiXwJck_Pb5Nj-wIrsqg',
+        channelName: 'YourMomsHousePodcast',
+        storeUrl: 'https://store.ymhstudios.com/collections/all-products',
+    },
+    {
+        channelId: 'UC5AQEUAwCh1sGDvkQtkDWUQ',
+        channelName: 'Theo Von',
+        storeUrl: 'https://www.theovonstore.com', 
+    },
+    {
+        channelId: 'UCTeIxzkL9QZ2noyLVUNfXJg',
+        channelName: 'Chris D\'Elia',
+        storeUrl: 'https://store.chrisdelia.com', 
+    },
+    {
+        channelId: 'UC6AbsTfBMQ_dHjtipwh3bZg',
+        channelName: 'The Fighter and The Kid',
+        storeUrl: 'https://thefighterandthekidshop.com/', 
+    },
+    {
+        channelId: 'UCzQUP1qoWDoEbmsQxvdjxgQ',
+        channelName: 'PowerfulJRE',
+        storeUrl: 'https://www.higherprimate.com/', 
+    },
+    {
+        channelId: 'UC5tigjL4SYA_nWP-E8-eiBw',
+        channelName: 'Trash Tuesday',
+        storeUrl: 'https://trash-tuesday.myshopify.com/',
+    },
+    {
+        channelId: 'UCYCGsNTvYxfkPkfQopRMP7w',
+        channelName: 'Rick Glassman',
+        storeUrl: 'https://www.rickglassman.com/store',
+    },
+    {
+        channelId: 'UCNGbPFX8UOm7qk6kvnHKr0w',
+        channelName: 'Andrew Santino',
+        storeUrl: 'https://www.andrewsantinostore.com',
+    },
+    {
+        channelId: 'UCLZc32yrTEMxH1ZO-6fKOzA',
+        channelName: 'The Andrew Schulz',
+        storeUrl: 'https://fashun.shop', 
+    },
+    {
+        channelId: 'UCmUsedCabQ7ylB8mL38NYXw',
+        channelName: 'King and the Sting',
+        storeUrl: 'https://www.katsmerch.com', 
+    },
+];
+
+// add more copy this 
+// {
+//     channelId: '',
+//     channelName: '',
+//     storeUrl: '', 
+// },
+
+// comedyPodcasts.forEach(youtuber => {
+//     db.query(
+//         'INSERT INTO Youtubers (channel_id, channel_name, store_url) VALUES (?, ?, ?)',
+//         [youtuber.channelId, youtuber.channelName, youtuber.storeUrl],(err, result) => {
+//             if (err) {
+//                 console.log(err);
+//             }
+//         }
+//     )
+// }); 
 
 const app = express(); 
 
@@ -40,12 +95,25 @@ app.get('/', (req, res) => {
     res.json('Welcome to my Youtuber Merch Api');
 });
 
-app.get('/youtubers', (req, res) => {
-    res.json(youtubers); 
+app.get('/comedy-podcasts', (req, res) => {
+    res.json(comedyPodcasts); 
 });
 
-// fetch data from merch stores 
+// insert data into table 
+function insertProductData(products) {
+    products.forEach(product => {
+        db.query(
+            'INSERT INTO YoutubeMerch (product_id, channel_id, name, price, img_src) VALUES (?, ?, ?, ?, ?)',
+            [product.id, product.channelId, product.name, product.price, product.imgSrc],(err, result) => {
+                if (err) {
+                    console.log(err);
+                }
+            }
+        )
+    }); 
+}
 
+// fetch data from merch stores 
 app.get('/ScissorBros/products', cors(), async (req, res) => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
@@ -56,8 +124,9 @@ app.get('/ScissorBros/products', cors(), async (req, res) => {
 
             return {
                 id: '', 
+                channelId: 'UCGCXWfqaovf_vI05iW6pLdA', 
                 name: item.querySelector('p[class="product-name m14"]').textContent,
-                price: item.querySelector('p[class="product-price ng-star-inserted"]').textContent,
+                price: item.querySelector('p[class="product-price ng-star-inserted"]').textContent.replace(/[\s+\$]/g, ''),
                 imgSrc: item.querySelector('img[class="product-art-image ng-star-inserted"]').src, 
             }
         })
@@ -66,29 +135,8 @@ app.get('/ScissorBros/products', cors(), async (req, res) => {
     // Each product id property is assigned a random id
     products.forEach(product => product.id = uuidv4());
 
-    res.json(products);
-    browser.close();
-});
-
-app.get('/PewDiePie/products', cors(), async (req, res) => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('https://represent.com/store/pewdiepie'); 
-    
-    const products = await page.evaluate(() => (
-        [...document.querySelectorAll('div[class="_product mb-8 mx-1 md:mb-12 md:mx-4"]')].map(item => {
-
-            return {
-                id: '', 
-                name: item.querySelector('div[class="_product-name mb-2 text-sm md:text-base"]').textContent,
-                price: item.querySelector('div[class="flex flex-row items-end mb-1 font-semibold"] > div').textContent,
-                imgSrc: item.querySelector('img[class="w-full h-full object-contain duration-200 transition-opacity opacity-100"]').src, 
-            }
-        })
-    ));
-    
-    // Each product id property is assigned a random id
-    products.forEach(product => product.id = uuidv4());
+    // Iterate through products and insert property values into table
+    insertProductData(products); 
 
     res.json(products);
     browser.close();
@@ -102,18 +150,25 @@ app.get('/TigerBelly/products', cors(), async (req, res) => {
         console.log(`scraping: ${url}`); 
         const productsOnPage = await page.evaluate(() => (
             [...document.querySelectorAll('div[class="grid__item large--one-quarter medium--one-half"]')].map(item => {
-    
+                
                 return {
                     id: '', 
                     name: item.querySelector('span[class="product-grid-title"]').textContent,
-                    price: item.querySelector('p[class="price"]').innerText,
+                    price: item.querySelector('p[class="price"]').textContent,  
                     imgSrc: item.querySelector('div[class="grid__image"] > img').src, 
                 }
             })
         ));
 
         // Each product id property is assigned a random id
-        productsOnPage.forEach(product => product.id = uuidv4());
+        productsOnPage.forEach(product => {
+            const pattern = /[A-Za-z\s+\$]/g; 
+            product.price = product.price.replace(pattern, ''); 
+            
+            if (product.price.length === 10) product.price = product.price.slice(0, -5);
+    
+            product.id = uuidv4(); 
+        });
 
         await page.close();
 
@@ -129,133 +184,11 @@ app.get('/TigerBelly/products', cors(), async (req, res) => {
     const url = 'https://tigerbelly.myshopify.com/collections/all?page=1'; 
     const products = await extractProducts(url); 
 
+    // Iterate through products and insert property values into table
+    insertProductData(products); 
+
     res.send(products);
     browser.close();
 });
 
-app.get('/OneyPlays/products', cors(), async (req, res) => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('https://sharkrobot.com/collections/oney'); 
-    
-    const products = await page.evaluate(() => (
-        [...document.querySelectorAll('.block-grid li')].map(item => {
-
-            return {
-                id: '', 
-                name: item.querySelector('a').title,
-                price: item.querySelector('.product-title em .money').textContent.replace(/[^\d\.]*/g, ''),
-                imgSrc: item.querySelector('img').src, 
-            }
-        })
-    ));
-    
-    // Each product id property is assigned a random id
-    products.forEach(product => product.id = uuidv4());
-
-    // Iterate through products and insert property values into table
-    products.forEach(product => {
-        db.query(
-            'INSERT INTO YoutubeMerch (product_id, name, price, img_src) VALUES (?, ?, ?, ?)',
-            [product.id, product.name, product.price, product.imgSrc],(err, result) => {
-                if (err) {
-                    console.log(err);
-                }
-            }
-        )
-    });
-
-    res.json(products);
-    browser.close();
-});
-
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
-
-// app.get('/', cors(), async (req, res) => {
-//     const browser = await puppeteer.launch();
-//     const page = await browser.newPage();
-//     await page.goto('https://sharkrobot.com/collections/oney');
-
-//     const product = await page.evaluate(() => {
-//         let items = [...document.querySelectorAll('div[class*="product" i] a')].map(item => {
-//             return {
-//                 title: item.textContent,
-//             }
-//         });
-
-//         return items 
-//     });
-    
-//     res.send(product);
-//     console.log(product);
-// });
-// a[href*="products" i]
-
-// app.get('/products', async (req, res) => {
-//     const response = await axios.get('https://represent.com/store/pewdiepie'); 
-//     const html = response.data;
-//     const $ = cheerio.load(html);
-
-//     $('a').each(() => {
-//         const title = $(this).text();
-//         const url = $(this).attr('href'); 
-//         products.push({
-//             title,
-//             url,
-//         })
-//     });
-
-//     console.log(products);
-//     res.send(products);
-// });
-
-// app.get('/h3h3Productions/products', cors(), async (req, res) => {
-//     const browser = await puppeteer.launch();
-//     const page = await browser.newPage();
-//     await page.goto('https://teddyfresh.com'); 
-    
-//     const products = await page.evaluate(() => (
-//         [...document.querySelectorAll('.ProductItem')].map(item => {
-
-//             return {
-//                 id: '',
-//                 title: item.querySelector('a').textContent,
-//                 url: item.querySelector('a').href,
-//                 imgSrc: item.querySelector('img').src
-//             }
-//         })
-//     ));
-    
-//     // Each product id property is assigned a random id
-//     products.forEach(product => product.id = uuidv4());
-
-
-//     res.json(products);
-//     browser.close();
-// });
-
-// app.get('/PewDiePie/products', cors(), async (req, res) => {
-//     const browser = await puppeteer.launch();
-//     const page = await browser.newPage();
-//     await page.goto('https://represent.com/store/pewdiepie'); 
-    
-//     const products = await page.evaluate(() => (
-//         [...document.querySelectorAll('._store_product_link')].map(item => {
-
-//             return {
-//                 id: '',
-//                 title: item.querySelector('a').title,
-//                 url: item.querySelector('a').href,
-//                 imgSrc: item.querySelector('img').src,
-//                 price: document.querySelector('h3').textContent
-//             }
-//         })
-//     ));
-    
-//     // Each product id property is assigned a random id
-//     products.forEach(product => product.id = uuidv4());
-
-//     console.log(products);
-//     res.json(products);
-//     browser.close();
-// });
---
+++ sql/schema.sql
@@ -0,0 +1,18 @@
+CREATE TABLE Youtubers (
+	id INT NOT NULL,
+	channel_id UNIQUE VARCHAR(50) NOT NULL,
+	channel_name VARCHAR(50) NOT NULL,
+	store_url VARCHAR(100) NOT NULL, 
+    PRIMARY KEY (id),
+);
+
+CREATE TABLE YoutuberMerch (
+    id INT NOT NULL,
+    channel_id VARCHAR(100) NOT NULL,
+    product_id VARCHAR(50) NOT NULL,
+    title VARCHAR(100) NOT NULL, 
+    price DECIMAL(10, 0),
+    img_src VARCHAR(150) NOT NULL, 
+    PRIMARY KEY (id),
+    FOREIGN KEY (channel_id) REFERENCES Youtubers(channel_id)
+);
