95a18346eb88cbcca0a0d1fdef4fdb3c329e3be3
web-dev <nathankim137311@gmail.com>
2022-01-04 23:50:25 -0800

    added env variable to launch.json file, removed config.js file

--- config.js
+++
@@ -1,5 +0,0 @@
-const config = {
-    DB_PASSWORD: 'uwApplicant731121', 
-}
-
-module.exports = { config };
--- database.js
+++ database.js
@@ -4,7 +4,7 @@ const { config } = require('./config.js');
 const db = mysql.createConnection({
     user: 'root', 
     host: '35.230.83.237', 
-    password: config.DB_PASSWORD, 
+    password: process.env.DB_PASSWORD, 
     database: 'YoutuberMerchApi',
 });
 
